c574e62970a9fcfd9145d900b8a15e9911b3ab20
nocekt <nocekt@gmail.com>
2016-09-12 16:06:52 +0200

    Fixed paths.

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js'
+  '/worker',
+  '/worker/styles/main.css',
+  '/worker/scripts/main.js'
 ];
 
 self.addEventListener('install', function(event) {
