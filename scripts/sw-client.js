b09b83bdd522662e0954d55faf5468994b201cfa
nocekt <nocekt@gmail.com>
2017-04-24 00:33:40 +0200

    Added proper logging for service worker.

--- scripts/sw-client.js
+++ scripts/sw-client.js
@@ -1,17 +1,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js').then(function(registration) {
-	// Registration was successful
-	console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
   }).catch(function(err) {
-	// registration failed :(
-	console.log('ServiceWorker registration failed: ', err);
+		console.log('ServiceWorker registration failed: ', err);
   });
-}
-
-var update = location.search.split('update=')[1];
-if(update == true) {
-	console.log("Update needed.");
-}
-else {
-	console.log("Up to date.");
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'getItDoneMainCache';
+
 var urlsToCache = [
   '/worker/',
   '/worker/styles/main.css',
@@ -6,31 +7,33 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+	console.log("Service worker install event: started");
+  	event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+		console.log("Service worker install event: success");
+		return cache.addAll(urlsToCache);
+	}));
+});
+
+self.addEventListener('activate', event => {
+  	console.log('Service worker activate event: activated');
 });
 
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(resp) {
+	event.respondWith(caches.match(event.request).then(function(resp) {
 		if(resp) {
-			console.log("Fetched: " + resp.url);
+			console.log("Service worker fetch event: served from cache - " + resp.url);
 			return resp;
 		}
 		fetch(event.request).then(function(response) {
-			return caches.open('v1').then(function(cache) {
-				cache.put(event.request, response.clone());
-				return response;
+			return caches.open(CACHE_NAME).then(function(cache) {
+				return cache.put(event.request, response.clone()).then(function() {
+					console.log("Service worker fetch event: downloaded -" + resp.url);
+				});
 			});  
-		});
-	})
-  );
+		})
+	}).catch(function(e) {
+		console.log("Service worker fetch event: failed to download -  " + resp.url);	
+	}));
 });
 
 self.addEventListener('message', function(event) {
